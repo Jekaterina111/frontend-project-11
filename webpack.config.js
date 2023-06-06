41507ca58edd48576e2aef96d266f086b7a09701
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-06-06 16:09:29 +0300

    add bootstrap and config webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(dirname, 'dist'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
+
+export default () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
