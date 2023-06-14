6ae8cff84b1f04ec3ab22608d9f3c4bce78e2a26
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-06-14 14:06:55 +0300

    fix linter mistakes

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
-    filename: "[name].[contenthash].js",
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
@@ -20,7 +20,7 @@ module.exports = {
       template: 'index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
+      filename: '[name].[contenthash].css',
     }),
   ],
   module: {
@@ -29,8 +29,10 @@ module.exports = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
-      { test: /\.css$/, 
-        use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
     ],
   },
   optimization: {
