e4a913ddd0b18c26d268e0467f9af7c8512f48c4
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-09-12 12:25:19 +0300

    add en.js with English text

---
+++ src/locales/en.js
@@ -0,0 +1,31 @@
+export default {
+  translation: {
+    languages: {
+      en: 'English',
+      ru: 'Русский',
+    },
+    form: {
+      submit: 'Add',
+      loading: 'Loading...',
+    },
+    mainTitle: 'RSS aggregator',
+    btnRead: 'Read completely',
+    label: 'RSS Link',
+    greeting: 'Receive notifications of new content in a comfortable format.',
+    success: 'RSS uploaded successfully',
+    posts: 'Posts',
+    feeds: 'Feeds',
+    button: 'View',
+    modalBtn: 'close',
+    sample: 'For example: https://ru.hexlet.io/lessons.rss',
+    errors: {
+      validation: {
+        alreadyExist: 'RSS already exists',
+        invalidRss: 'The resource doesn\'t contain valid RSS',
+        network: 'Network error',
+        empty: 'Must not be empty',
+        invalidUrl: 'Link must be a valid URL',
+      },
+    },
+  },
+};
